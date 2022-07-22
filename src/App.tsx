23f6ca18c5c6d3ced2beaecb02f9b581bc144fc1
bsharpnoob <bsharpnoob@users.noreply.github.com>
2022-07-22 16:30:47 +1000

    created Card and Popup components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TextField from '@mui/material/TextField';
+
 function App() {
+
+	const search = () => {
+
+	}
+
+
   return (
-    <div className="App">
-      
+    <div>
+      <TextField
+				variant='filled'
+				size='small'
+				label="Search for coin..."
+      >
+			<button onClick={search}></button>
+
+
+      </TextField>
     </div>
   );
 }
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+
+export default function Card() {
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
